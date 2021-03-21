bf8084b643b400ff3cf1a15522c1ce1859a8955a
t o <t.o@gmail.com>
2021-03-21 22:36:22 +0900

    update

--- sample4/target/debug/deps/sample4-018590d8cae7e323.d
+++
@@ -1,5 +0,0 @@
-/home/toru/rust-learning/sample4/target/debug/deps/sample4-018590d8cae7e323.rmeta: src/main.rs
-
-/home/toru/rust-learning/sample4/target/debug/deps/sample4-018590d8cae7e323.d: src/main.rs
-
-src/main.rs:
--- sample4/target/debug/deps/sample4-afd79f3b5158e85f.d
+++
@@ -1,5 +0,0 @@
-/home/toru/rust-learning/sample4/target/debug/deps/sample4-afd79f3b5158e85f.rmeta: src/main.rs
-
-/home/toru/rust-learning/sample4/target/debug/deps/sample4-afd79f3b5158e85f.d: src/main.rs
-
-src/main.rs:
