a1c864788ed713da933b0124dcabe64fe6966a9b
t o <t.o@gmail.com>
2021-03-22 15:07:28 +0900

    update

--- sample15/src/main.rs
+++ sample15/src/main.rs
@@ -17,3 +17,26 @@ fn test_add() {
     assert_eq!(1, add(1, 0));
     assert_eq!(2, add(1, 1));
 }
+
+#[test]
+fn assert_sample() {
+    assert!(true);
+    assert!(false, "panic! value={}", false);
+
+    assert_eq!(true, true);
+    assert_ne!(true, false);
+
+    assert_eq!(true, false, "panic! value ({}, {})", true, false);
+}
+
+#[test]
+#[should_panic]
+fn test_panic() {
+    panic!("expected panic");
+}
+
+#[test]
+#[ignore]
+fn test_add_ignored() {
+    assert_eq!(-2, add(-1, -1));
+}
